55922250568ba5c8254c89754ca02a47479c5718
Prasoon <prasoonpandey06@gmail.com>
2018-03-04 19:59:16 +0530

    Create Launcher.java

---
+++ Launcher.java
@@ -0,0 +1,10 @@
+/*
+ * This class is the entry point and the launcher of the Timer class.
+   */
+class Launcher
+{
+    public static void main (String args[])
+    {
+        new Timer();
+    }
+}
