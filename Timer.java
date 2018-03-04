e2aac6e560781755c20bfcf2c6676bb3124764f5
Prasoon <prasoonpandey06@gmail.com>
2018-03-04 20:15:08 +0530

    Create a window and update package.bluej

--- Timer.java
+++ Timer.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 class Timer
-{}
+{
+    //Declarations
+    private JFrame frame;
+    
+    Timer()
+    {
+        //Initialisations
+        frame = new JFrame("GammaTimer");
+        
+        //Setting up
+        frame.setVisible(true);
+        frame.setSize(450, 450);
+    }
+}
