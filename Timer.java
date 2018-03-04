07d75bdc6ae8bd0ccf1b33f77a9bb4c7d125cbc8
Prasoon <prasoonpandey06@gmail.com>
2018-03-04 20:27:30 +0530

    Add control buttons

--- Timer.java
+++ Timer.java
@@ -4,14 +4,26 @@ class Timer
 {
     //Declarations
     private JFrame frame;
+    private JPanel ctrl;
+    private JButton b1, b2;
     
     Timer()
     {
         //Initialisations
         frame = new JFrame("GammaTimer");
+        ctrl = new JPanel();
+        b1 = new JButton("Set Timer");
+        b2 = new JButton("Stop Timer");
         
         //Setting up
         frame.setVisible(true);
         frame.setSize(450, 450);
+        b1.setSize(50, 10);
+        b2.setSize(50, 10);
+        
+        //Adding to frame
+        ctrl.add(b1);
+        ctrl.add(b2);
+        frame.add(ctrl);
     }
 }
