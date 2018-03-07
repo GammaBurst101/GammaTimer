b1267aa2bd0b3abd4ab3c0c5561d8a6d8ae7e3c9
Prasoon <prasoonpandey06@gmail.com>
2018-03-07 16:28:51 +0530

    Refactor the code

--- Timer.java
+++ Timer.java
@@ -6,7 +6,7 @@ class Timer
     //Declarations
     private JFrame frame;
     private JPanel ctrl, input;
-    private JButton b1, b2;
+    private JButton setButton, stopButton;
     private JTextField inputHr, inputMin, inputSec;
     private JLabel inputMsg;
     
@@ -16,8 +16,8 @@ class Timer
         frame = new JFrame("GammaTimer");
         ctrl = new JPanel();
         input = new JPanel();
-        b1 = new JButton("Set Timer");
-        b2 = new JButton("Stop Timer");
+        setButton = new JButton("Set Timer");
+        stopButton = new JButton("Stop Timer");
         inputHr = new JTextField("Hr");
         inputMin = new JTextField("Min");
         inputSec = new JTextField("Sec");
@@ -27,19 +27,19 @@ class Timer
         frame.setVisible(true);
         frame.setSize(450, 450);
         frame.setLayout(new FlowLayout());
-        b1.setSize(50, 10);
-        b2.setSize(50, 10);
+        setButton.setSize(50, 10);
+        stopButton.setSize(50, 10);
         inputHr.setSize(2, 4);
         inputMin.setSize(2, 4);
         inputSec.setSize(2, 4);
         
         //Adding to panels
-        ctrl.add(b1);
-        ctrl.add(b2);
+        ctrl.add(setButton);
+        ctrl.add(stopButton);
+        input.add(inputMsg);
         input.add(inputHr);
         input.add(inputMin);
         input.add(inputSec);
-        input.add(inputMsg);
         
         //panel config
         input.setBounds(10, 10, 200, 200);
