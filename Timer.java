f5532c269488a3f2d28afe2564ae9fe768e99948
Prasoon <prasoonpandey06@gmail.com>
2018-03-11 19:23:20 +0530

    Use setBounds() instead of panels and update package.bluej

--- Timer.java
+++ Timer.java
@@ -7,7 +7,6 @@ class Timer implements ActionListener
 {
     //Declarations
     private JFrame frame;
-    private JPanel ctrl, input, timer;
     private JButton setButton, stopButton;
     private JTextField inputHr, inputMin, inputSec, display;
     private JLabel inputMsg;
@@ -17,27 +16,28 @@ class Timer implements ActionListener
     {
         //Initialisations
         frame = new JFrame("GammaTimer");
-        ctrl = new JPanel();
-        input = new JPanel();
-        timer = new JPanel();
         setButton = new JButton("Set Timer");
         stopButton = new JButton("Stop Timer");
         inputHr = new JTextField("Hr");
         inputMin = new JTextField("Min");
         inputSec = new JTextField("Sec");
-        display = new JTextField(400);
+        display = new JTextField(100);
         inputMsg = new JLabel("Enter the time here");
         
         //Setting up
         frame.setVisible(true);
         frame.setSize(450, 450);
-        frame.setLayout(new FlowLayout());
-        setButton.setSize(50, 10);
-        stopButton.setSize(50, 10);
-        inputHr.setSize(2, 4);
-        inputMin.setSize(2, 4);
-        inputSec.setSize(2, 4);
+        frame.setLayout(null);
         display.setEditable(false);
+        
+        //Using setBounds()
+        display.setBounds(10, 10, 400, 100);
+        inputMsg.setBounds (10, 100, 400, 50);
+        inputHr.setBounds (40, 150, 50, 15);
+        inputMin.setBounds (100, 150, 50, 15);
+        inputSec.setBounds (160, 150, 50, 15);
+        setButton.setBounds (20, 200, 100, 30);
+        stopButton.setBounds (200, 200, 100, 30);
         
         //Registering ActionListener
         setButton.addActionListener(this);
@@ -50,24 +50,14 @@ class Timer implements ActionListener
         }
         );
         
-        //Adding to panels
-        timer.add(display);
-        ctrl.add(setButton);
-        ctrl.add(stopButton);
-        input.add(inputMsg);
-        input.add(inputHr);
-        input.add(inputMin);
-        input.add(inputSec);
-        
-        //panel config
-        timer.setBounds(10, 10, 410, 60);
-        input.setBounds(10, 10, 200, 200);
-        ctrl.setBounds(300, 20, 300, 50);
-        
         //Adding to frame
-        frame.add(timer);
-        frame.add(input);
-        frame.add(ctrl);
+        frame.add(display);
+        frame.add(inputMsg);
+        frame.add(inputHr);
+        frame.add(inputMin);
+        frame.add(inputSec);
+        frame.add(setButton);
+        frame.add(stopButton);
     }
     
     @Override
